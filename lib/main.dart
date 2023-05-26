709a562bec77b307bca9879f98f47175efb6f726
Ahmed A. Khamas <abas93931@gmail.com>
2023-05-27 01:04:09 +0300

    MAIN RELEASE changed icons

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Qr scanner',
       theme: ThemeData(
         brightness: Brightness.dark,
         useMaterial3: true,
